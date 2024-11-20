a92da5d0d8f81419e57f1cdef3c293b18f57fc04
Ji998 <xij1@hi.is>
2024-11-20 23:56:11 +0000

    hi

---
+++ main.js
@@ -0,0 +1,51 @@
+import {el} from "./lib/elements.js";
+
+async function fetchIndex(){
+    const file ='public/data/index.json';
+
+    const response =await fetch(file);
+    const json=await response.json();
+    
+    return json;
+}
+
+
+
+async function render(root){
+    const indexJson =await fetchIndex();
+    console.log('rendering',root, indexJson.title);
+   
+/*
+<nav>
+<a href="${url}">${title}</a>
+<a href="${url}">${title}</a>
+<a href="${url}">${title}</a>
+</nav>
+
+*/   
+const navigationElement=el('nav',{});
+
+for(const item of indexJson.navigation){
+    const{title ,slug}=item;
+   const href='#${slug}';
+    const navItemElement=el('a',{href},title);
+navItemElement.appendChild(navItemElement);
+}
+headerElement.appendChild(navigationElement);
+
+
+
+    const headerElement =el('header',{},el('h1',{},indexJson.title)) ;
+const mainElement =el('main',{},el('h2',{},el('section',{},el('p',{},indexJson.description)),));
+const footerElement=el('footer',{},indexJson.footer)
+   root.appendChild(headerElement);
+   root.appendChild(mainElement)
+   root.appendChild(footerElement);
+
+}
+
+
+
+const root =document.querySelector('#app');
+
+render(root);
