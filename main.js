04d0fce27685677d1e1168546be9385a46c2cf14
dannith <dannitorr@gmail.com>
2024-11-22 23:02:04 +0000

    laga questions og keywords

--- main.js
+++ main.js
@@ -4,24 +4,25 @@ import { renderIndexPage } from './lib/pages/index-page.js';
 import { renderSubpage } from './lib/pages/sub-page.js';
 
 async function render(root, querystring) {
-  const mainIndexJson = await fetcher('/index.json');
+  const mainIndexJson = await fetcher('data/index.json');
+
   const params = new URLSearchParams(querystring);
   const type = params.get('type');
   const content = params.get('content');
+
+  console.log(type, content);
 
   if (!type) {
     return renderIndexPage(root, mainIndexJson);
   }
 
   if (content) {
-    return renderContentPage(root, mainIndexJson);
+    return renderContentPage(root, mainIndexJson, content);
   }
 
   renderSubpage(root, mainIndexJson, type);
 }
 
 const root = document.querySelector('#app');
-window.addEventListener('popstate', () => {
-  render(root, window.location.search);
-});
+
 render(root, window.location.search);
